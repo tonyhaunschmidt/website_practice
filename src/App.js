ef99575d59a77abd46945ac0a09bf2e523bfb558
Tony Haunschmidt <tmhaunschmidt@gmail.com>
2022-05-26 16:44:37 +0100

    sections

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React from 'react'
 
 function App() {
   return (
-    <></>
+    <>
+      <header></header>
+      <section className='hero'></section>
+      <section className='welcome'></section>
+      <section className='events'></section>
+      <section className='social'></section>
+      <section className='gallery'></section>
+      <section className='map'></section>
+      <section className='accreditations'></section>
+      <section className='contact'>
+        <div className='find_us'></div>
+        <div className='contact_us'></div>
+        <div className='partnership'></div>
+      </section>
+      <footer></footer>
+    </>
+
   );
 }
 
